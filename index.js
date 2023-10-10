a5cdb47ebfc7448f3906b9821482fdfe09a8fcf0
Matthew Houston <matthouston027@gmail.com>
2023-10-09 20:56:51 -0500

    Add comment from new endpoint to current comments and render html

--- index.js
+++ index.js
@@ -1,23 +1,57 @@
 const express = require('express');
 const app = express();
+const path = require('path')
 
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true }));  //For form encoded data
 app.use(express.json()); //For parsing json data
 
-app.get('/', (req, res) => {
-    res.render("index.ejs");
-})
-app.get('/tacos', (req, res) => {
-    console.log("Got GET Request", req.body)
+const comments = [
+    {
+        username: 'Todd',
+        comment: 'lol that is so funny'
+    },
+    {
+        username: 'Skyler',
+        comment: 'I like to go birdwatching with my dog'
+    },
+    {
+        username: 'Sk8terBoi',
+        comment: 'Please delete your account, Todd'
+    },
+    {
+        username: 'onlysayswoof',
+        comment: 'woof woof woof'
+    },
+]
 
-    res.send("GET /tacos response")
+app.get('/comments', (req, res) => {
+    res.render('comments/index', { comments });
 })
-app.post('/tacos', (req, res) => {
-    console.log("Got POST Request", req.body)
-    const { meat, qty } = req.body;
-    res.send(`Ok! Here are your ${qty} ${meat} tacos`)
+app.get('/comments/new', (req, res) => {
+    res.render('comments/newComment');
 })
+
+app.post('/comments', (req, res) => {
+    const { username, comment } = req.body;
+    comments.push({ username, comment })
+    res.render('comments/index', { comments });
+})
+
+// app.get('/', (req, res) => {
+//     res.render("index.ejs");
+// })
+// app.get('/tacos', (req, res) => {
+//     console.log("Got GET Request", req.body)
+
+//     res.send("GET /tacos response")
+// })
+// app.post('/tacos', (req, res) => {
+//     console.log("Got POST Request", req.body)
+//     const { meat, qty } = req.body;
+//     res.send(`Ok! Here are your ${qty} ${meat} tacos`)
+// })
 
 app.listen(3000, () => {
     console.log("On port 3000")
